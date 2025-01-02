8dbbd6a9273ce1c26b58315bb78c5479023e26ce
Jiveetesh Mourya <jcodestudy@gmail.com>
2025-01-02 15:50:15 +0530

    ad

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 // import {holdings} from "../data/data";
-import axios, {all} from "axios";
+import axios from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
 const Holdings = () => {
